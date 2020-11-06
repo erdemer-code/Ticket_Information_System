1e3fead67e1ab3e953a8af5051214444db9465b8
erdemer-code <72577298+erdemer-code@users.noreply.github.com>
2020-11-06 22:47:42 +0300

    User SignIn/LogIn Screen Connected with DB +Added some functionality in signup system

--- src/Controllers/SignUpController.java
+++ src/Controllers/SignUpController.java
@@ -6,8 +6,10 @@
 package Controllers;
 
 import Models.User;
+import java.util.List;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
@@ -36,14 +38,37 @@ public class SignUpController{
     void addUser(ActionEvent event) {
         boolean studentStatus = false;
         UserDAOImpl dAOImpl = new UserDAOImpl();
+        List<User> userList = dAOImpl.getAllUsers();
+        boolean isMatched = false;
         
         if(studentCheckBox.isSelected()){
             studentStatus = true;
         }
         
+        for(User user : userList){
+            if(user.getName().equals(signUpName.getText())){
+                isMatched = true;
+            }
+        }
+        if(!isMatched){
         User user = new User(signUpName.getText(),signUpPassword.getText(),studentStatus);
-        
         dAOImpl.insertUser(user);
+        } else {
+            showErrorMessage("This username has already taken, please write another username and password!");
+            signUpName.setText("");
+            signUpPassword.setText("");
+        }
+        
+        
+    }
+    
+     private void showErrorMessage(String msg) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Information Dialog");
+        alert.setHeaderText(null);
+        alert.setContentText(msg);
+
+        alert.showAndWait();
     }
     
 }
